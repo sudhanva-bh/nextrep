22c41ece59c3b375b2fdc797eaad3d0b5094a109
sudhanva-bh <lonelycube123@gmail.com>
2025-09-10 00:09:47 +0530

    Fix Edit Bug

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,9 @@
-// // php -S localhost:8000 -c C:\php\php.ini-development api.php
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nextrep/auth_wrapper.dart';
-import 'package:nextrep/core/entities/favourite_workouts/favourite_workout.dart';
-import 'package:nextrep/core/services/challenges/challenges_service.dart';
-import 'package:nextrep/core/services/exercises/workouts_service.dart';
-import 'package:nextrep/core/services/favourite_workouts/favourite_workouts_service.dart';
 import 'package:nextrep/core/services/hive_init_service.dart';
 import 'package:nextrep/core/theme/theme.dart';
 import 'package:nextrep/firebase_options.dart';
@@ -64,68 +58,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// import 'package:flutter/material.dart';
-// import 'package:flutter_dotenv/flutter_dotenv.dart';
-// import 'package:nextrep/core/services/exercises/exercise_raw_data_service.dart';
-// import 'package:nextrep/core/services/hive_init_service.dart';
-// import 'package:nextrep/core/theme/app_palette.dart';
-// import 'package:nextrep/features/preview_workout/presentation/data/fetch_target_part_images.dart';
-// import 'package:nextrep/features/preview_workout/presentation/utils/bottom_workout_preview_popup.dart';
-// import 'package:nextrep/features/preview_workout/presentation/utils/consequtive_images.dart';
-
-// Future<void> main() async {
-//   await HiveInitService.initHive();
-
-//   await dotenv.load(fileName: ".env");
-//   runApp(const TestApp());
-// }
-
-// class TestApp extends StatelessWidget {
-//   const TestApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       theme: ThemeData.dark().copyWith(
-//         scaffoldBackgroundColor: AppPalette.background,
-//       ),
-//       home: const TestYouTubeEmbed(),
-//     );
-//   }
-// }
-
-// class TestYouTubeEmbed extends StatelessWidget {
-//   const TestYouTubeEmbed({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final exercise = ExerciseService().getExerciseById("0020");
-//     return Scaffold(
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             // SizedBox(
-//             //   width: 400,
-//             //   child: YouTubeEmbed(
-//             //     exercise: ExerciseService().getExerciseById("0416"),
-//             //   ),
-//             // ),
-//             MuscleApiService().getMuscleImageWidgetBuilder(exercise),
-//             ElevatedButton(
-//               onPressed: () => BottomWorkoutPreviewPopup(
-//                 exercise: exercise,
-//               ).showPopup(context),
-//               child: Icon(Icons.abc),
-//             ),
-//             ConsecutiveImages(
-//               firstImagePath: exercise.image0,
-//               secondImagePath: exercise.image1,
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
